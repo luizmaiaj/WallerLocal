aa9a3c9ce23942f767239c177a39db8316393f5b
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2025-01-30 18:18:50 +0700

    Creatting robot gp

---
+++ robot_gp.hpp
@@ -0,0 +1,128 @@
+#ifndef ROBOT_GP_HPP
+#define ROBOT_GP_HPP
+
+#include "gp_engine.hpp"
+#include "robot.h"
+#include <variant>
+
+namespace robot_gp {
+
+// Command types
+enum class RobotCommand {
+    // Function nodes
+    PROGN3,     // Execute 3 commands in sequence
+    PROGN2,     // Execute 2 commands in sequence
+    IFWALL,     // Execute left if near wall, right otherwise
+    IFBALL,     // Execute left if ball visible, right otherwise
+    
+    // Terminal nodes
+    WALKFRONT,  // Move forward
+    WALKBACK,   // Move backward
+    LEFT,       // Turn left
+    RIGHT,      // Turn right
+    ALIGN       // Orient towards ball
+};
+
+// Node value that holds a robot command
+struct RobotNodeValue {
+    RobotCommand cmd;
+    
+    // Get number of children required for this command
+    [[nodiscard]] size_t children_count() const {
+        switch (cmd) {
+            case RobotCommand::PROGN3:
+                return 3;
+            case RobotCommand::PROGN2:
+            case RobotCommand::IFWALL:
+            case RobotCommand::IFBALL:
+                return 2;
+            default:
+                return 0;
+        }
+    }
+    
+    // Check if this is a function node
+    [[nodiscard]] bool is_function() const {
+        return children_count() > 0;
+    }
+    
+    // Get character representation (for compatibility with existing code)
+    [[nodiscard]] char to_char() const {
+        switch (cmd) {
+            case RobotCommand::PROGN3: return '3';
+            case RobotCommand::PROGN2: return '2';
+            case RobotCommand::IFWALL: return 'I';
+            case RobotCommand::IFBALL: return 'C';
+            case RobotCommand::WALKFRONT: return 'F';
+            case RobotCommand::WALKBACK: return 'B';
+            case RobotCommand::LEFT: return 'L';
+            case RobotCommand::RIGHT: return 'R';
+            case RobotCommand::ALIGN: return 'A';
+        }
+        return '?';
+    }
+    
+    // Create from character (for compatibility with existing code)
+    static RobotNodeValue from_char(char c) {
+        switch (c) {
+            case '3': return {RobotCommand::PROGN3};
+            case '2': return {RobotCommand::PROGN2};
+            case 'I': return {RobotCommand::IFWALL};
+            case 'C': return {RobotCommand::IFBALL};
+            case 'F': return {RobotCommand::WALKFRONT};
+            case 'B': return {RobotCommand::WALKBACK};
+            case 'L': return {RobotCommand::LEFT};
+            case 'R': return {RobotCommand::RIGHT};
+            case 'A': return {RobotCommand::ALIGN};
+            default: throw std::invalid_argument("Invalid robot command character");
+        }
+    }
+};
+
+// Evaluator class that executes robot commands
+class RobotEvaluator {
+private:
+    Robot& robot;
+    struct ball_data& ball;
+
+public:
+    RobotEvaluator(Robot& r, struct ball_data& b) : robot(r), ball(b) {}
+    
+    void execute_command(const RobotNodeValue& cmd) {
+        switch (cmd.cmd) {
+            case RobotCommand::WALKFRONT:
+                robot.walkFront();
+                break;
+            case RobotCommand::WALKBACK:
+                robot.walkBack();
+                break;
+            case RobotCommand::LEFT:
+                robot.turnLeft();
+                break;
+            case RobotCommand::RIGHT:
+                robot.turnRight();
+                break;
+            case RobotCommand::ALIGN:
+                robot.align(ball.lin, ball.col);
+                break;
+            default:
+                // Function nodes are handled by the GP engine
+                break;
+        }
+    }
+    
+    bool check_condition(const RobotNodeValue& cmd) {
+        switch (cmd.cmd) {
+            case RobotCommand::IFWALL:
+                return robot.isNearWall();
+            case RobotCommand::IFBALL:
+                return robot.canSeeBall(ball.lin, ball.col);
+            default:
+                return false;
+        }
+    }
+};
+
+} // namespace robot_gp
+
+#endif // ROBOT_GP_HPP
