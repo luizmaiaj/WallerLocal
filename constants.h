019bc994e70989d156ad94535a7cf11c436a5813
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2025-01-30 16:46:46 +0700

    refactor: moving constants to their own file

---
+++ constants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define GENS 51                    //NUMERO DE GERACOES
+#define POPULATION 500             //TAMANHO DA POPULACAO
+#define CROSSING 350               //70% DA POPULACAO
+#define REPRODUCTION 150           //30% DA POPULACAO
+#define HEIGHT 200                 //ALTURA DA MATRIZ
+#define WIDTH 200                  //LARGURA DA MATRIZ
+#define RUNS 1                     //NUMERO DE TESTES DE CADA INDIVIDUOS
+#define EXECUTE 2000               //NUMERO DE EXECUCOES DA ARVORE POR TESTE
+#define LIMIT 1000                 //LIMITA COMPRIMENTO DO INDIVIDUO
+#define ANGLE 5                    //ANGULO QUE O ROBO SE VIRA
+#define HIT_DISTANCE 1             //DISTANCIA CONSIDERADA PARA TOQUE
+#define VIEW_ANGLE 30              //DEFINE ANGULO DA VISAO LOCAL
--- main.cpp
+++ main.cpp
@@ -19,19 +19,7 @@
 
 #include "environment.h"
 #include "robot.h"
-
-#define GENS 51                    //NUMERO DE GERACOES
-#define POPULATION 500             //TAMANHO DA POPULACAO
-#define CROSSING 350               //70% DA POPULACAO
-#define REPRODUCTION 150           //30% DA POPULACAO
-#define HEIGHT 200                 //ALTURA DA MATRIZ
-#define WIDTH 200                  //LARGURA DA MATRIZ
-#define RUNS 1                     //NUMERO DE TESTES DE CADA INDIVIDUOS
-#define EXECUTE 2000               //NUMERO DE EXECUCOES DA ARVORE POR TESTE
-#define LIMIT 1000                 //LIMITA COMPRIMENTO DO INDIVIDUO
-#define ANGLE 5                    //ANGULO QUE O ROBO SE VIRA
-#define HIT_DISTANCE 1             //DISTANCIA CONSIDERADA PARA TOQUE
-#define VIEW_ANGLE 30              //DEFINE ANGULO DA VISAO LOCAL
+#include "constants.h"
 
 /*
  ******************************************************************************************************
@@ -1186,12 +1174,6 @@ void execute(struct tree* pointer, struct ball_data* ball)
 		break;
 	}
 }
-
-//*******************************
-//* RECEBE MATRIZ E A PREENCHE  *
-//* CONFORME O AMBIENTE INICIAL *
-//*******************************
-// Removed initializeEnvironment - now using Environment class initialize() method
 
 //****************************************
 //* RECEBE MATRIZ DE IMAGEM E A PREENCHE *
